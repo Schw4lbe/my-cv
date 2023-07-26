5f369e6c224864664b520b993b45023bc9f76c32
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-26 12:34:15 +0200

    created menu main component

---
+++ src/components/MenuMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="menu-container">
+    <div class="menu-item">Menu Item</div>
+    <div class="menu-item">Menu Item</div>
+    <div class="menu-item">Menu Item</div>
+    <div class="menu-item">Menu Item</div>
+    <div class="menu-item">Menu Item</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MenuMain",
+  props: {
+    menuItems: Array,
+  },
+};
+</script>
+
+<style></style>
