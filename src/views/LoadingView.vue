a19746ab85dc4a77c65a5939c6e94e10812594e9
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-27 10:20:10 +0200

    excluded all stylings into partials without optimization and nesting.

--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script>
-// @ is an alias to /src
 import MenuMain from "@/components/MenuMain.vue";
 import LoadingScreen from "@/components/LoadingScreen.vue";
 
@@ -25,61 +24,3 @@ export default {
   },
 };
 </script>
-<style scoped>
-/* .loading-header {
-  position: absolute;
-  width: 100%;
-  top: 65%;
-}
-.image-frame {
-  position: absolute;
-  width: 100%;
-  display: flex;
-
-  justify-content: center;
-  align-items: center;
-
-  animation-name: imageSlide;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-timing-function: ease-in;
-  animation-duration: 2s;
-  animation-delay: 10s;
-}
-
-@keyframes imageSlide {
-  0% {
-    transform: translateX(0);
-  }
-  100% {
-    transform: translateX(-35%);
-  }
-}
-
-img {
-  position: relative;
-  height: 65%;
-  width: 65%;
-}
-@keyframes profile {
-  0% {
-    border-radius: 0%;
-    height: 65%;
-    width: 65%;
-  }
-  100% {
-    border-radius: 100%;
-    height: 30%;
-    width: 30%;
-  }
-}
-.img-animation {
-  border-radius: 0%;
-  animation-name: profile;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-timing-function: ease-in;
-  animation-duration: 2s;
-  animation-delay: 10s;
-} */
-</style>
