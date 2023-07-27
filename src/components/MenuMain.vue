f594012a81976f2b4dad488214d9e37a4bb0c32d
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-27 10:45:59 +0200

    base animation for menu header

--- src/components/MenuMain.vue
+++ src/components/MenuMain.vue
@@ -1,10 +1,11 @@
 <template>
+  <div class="menu-header"><h3>Current Menu Item</h3></div>
   <div class="menu-container">
-    <div class="menu-item">Menu Item</div>
-    <div class="menu-item">Menu Item</div>
-    <div class="menu-item">Menu Item</div>
-    <div class="menu-item">Menu Item</div>
-    <div class="menu-item">Menu Item</div>
+    <div id="item1" class="menu-item">Menu Item</div>
+    <div id="item2" class="menu-item">Menu Item</div>
+    <div id="item3" class="menu-item">Menu Item</div>
+    <div id="item4" class="menu-item">Menu Item</div>
+    <div id="item5" class="menu-item">Menu Item</div>
   </div>
 </template>
 
