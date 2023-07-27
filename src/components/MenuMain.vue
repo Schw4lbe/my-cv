baa04768893f12cca2d1ea0e3286e38202bab376
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-27 21:47:51 +0200

    fixed all animations for now.

--- src/components/MenuMain.vue
+++ src/components/MenuMain.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="menu-header"><h3>Current Menu Item</h3></div>
-  <div class="menu-container">
-    <div class="menu-image-frame">
-      <img class="menu-image" alt="picture" src="../assets/dummy.png" />
-    </div>
-    <div
-      v-for="item in menuItems"
-      :key="item.id"
-      :id="item.id"
-      class="menu-item"
-    >
-      <a href="#">{{ item.name }}</a>
+  <div class="menu-frame">
+    <div class="menu-header"><h3>Persönliches</h3></div>
+    <div class="menu-item-container">
+      <div class="menu-image-frame">
+        <img class="menu-image" alt="picture" src="../assets/dummy.png" />
+      </div>
+      <div
+        v-for="item in menuItems"
+        :key="item.id"
+        :id="item.id"
+        class="menu-item"
+      >
+        <a href="#">{{ item.name }}</a>
+      </div>
     </div>
   </div>
 </template>
