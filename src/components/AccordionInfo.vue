fea15f6372f265cd6b712a2b977ac13ea3ebd6df
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-28 16:28:51 +0200

    created accordion component for info display.

---
+++ src/components/AccordionInfo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row">
+    <div class="col">
+      <div class="tabs">
+        <div class="tab">
+          <input type="checkbox" id="chck1" />
+          <label class="tab-label" for="chck1">Item 1</label>
+          <div class="tab-content">
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum,
+            reiciendis!
+          </div>
+        </div>
+        <div class="tab">
+          <input type="checkbox" id="chck2" />
+          <label class="tab-label" for="chck2">Item 2</label>
+          <div class="tab-content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. A, in!
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccordionInfo",
+  props: {
+    header: String,
+    items: Array,
+    media: String,
+  },
+};
+</script>
--- src/views/SalesView.vue
+++ src/views/SalesView.vue
@@ -1,10 +1,16 @@
 <template>
   <h1>Sales Erfahrung</h1>
+  <AccordionInfo></AccordionInfo>
 </template>
 
 <script>
+import AccordionInfo from "@/components/AccordionInfo.vue";
+
 export default {
   name: "SalesView",
+  components: {
+    AccordionInfo,
+  },
 };
 </script>
 
