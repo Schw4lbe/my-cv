7917a54130516aa9080d1829704ce41fab792d33
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-28 14:27:20 +0200

    moved personal data to personview new component.

--- src/views/LoadingView.vue
+++ src/views/LoadingView.vue
@@ -1,41 +1,16 @@
 <template>
   <div class="container">
     <LoadingScreen header="Lebenslauf" subHeader="Jean-Pierre Häussler" />
-    <PersonalData
-      :properties="propertiesArray"
-      :hobbies="hobbiesArray"
-      :skills="skillsArray"
-    ></PersonalData>
   </div>
 </template>
 
 <script>
 import LoadingScreen from "@/components/LoadingScreen.vue";
-import PersonalData from "@/components/PersonalData.vue";
 
 export default {
   name: "LoadingView",
   components: {
     LoadingScreen,
-    PersonalData,
-  },
-  data() {
-    return {
-      skillsArray: [
-        { id: "1", name: "html", value: "90" },
-        { id: "2", name: "css", value: "80" },
-      ],
-      propertiesArray: [
-        { id: "1", name: "Eigenmotivation" },
-        { id: "2", name: "Verantwortungsbewustsein" },
-        { id: "3", name: "Authentizität" },
-      ],
-      hobbiesArray: [
-        { id: "1", name: "Sport, fernöstliche Kampf- & Körperkunst" },
-        { id: "2", name: "Musik Produktion, DJ-ing & Livestreams" },
-        { id: "3", name: "E-Sport, Game Development (C# + Unity)" },
-      ],
-    };
   },
 };
 </script>
---
+++ src/views/PersonView.vue
@@ -0,0 +1,38 @@
+<template>
+  <PersonalData
+    :properties="propertiesArray"
+    :hobbies="hobbiesArray"
+    :skills="skillsArray"
+  ></PersonalData>
+</template>
+
+<script>
+import PersonalData from "@/components/PersonalData.vue";
+
+export default {
+  name: "PersonView",
+  components: {
+    PersonalData,
+  },
+  data() {
+    return {
+      skillsArray: [
+        { id: "1", name: "html", value: "90" },
+        { id: "2", name: "css", value: "80" },
+      ],
+      propertiesArray: [
+        { id: "1", name: "Eigenmotivation" },
+        { id: "2", name: "Verantwortungsbewustsein" },
+        { id: "3", name: "Authentizität" },
+      ],
+      hobbiesArray: [
+        { id: "1", name: "Sport, fernöstliche Kampf- & Körperkunst" },
+        { id: "2", name: "Musik Produktion, DJ-ing & Livestreams" },
+        { id: "3", name: "E-Sport, Game Development (C# + Unity)" },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
