f99122aae86cf915acb7c8d181868544920f9dce
TapeMate <admin@tapemate.onmicrosoft.com>
2023-07-28 17:17:34 +0200

    implemented accordion to other views.

--- src/views/CodingView.vue
+++ src/views/CodingView.vue
@@ -1,10 +1,32 @@
 <template>
   <h1>Coding Erfahrung</h1>
+  <AccordionInfo :items="codingItems"></AccordionInfo>
 </template>
 
 <script>
+import AccordionInfo from "@/components/AccordionInfo.vue";
+
 export default {
   name: "CodingView",
+  components: {
+    AccordionInfo,
+  },
+  data() {
+    return {
+      codingItems: [
+        {
+          id: "1",
+          header: "Header 1",
+          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+        },
+        {
+          id: "2",
+          header: "Header2",
+          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+        },
+      ],
+    };
+  },
 };
 </script>
 
--- src/views/FreelanceView.vue
+++ src/views/FreelanceView.vue
@@ -1,10 +1,32 @@
 <template>
   <h1>Selbstständigkeit</h1>
+  <AccordionInfo :items="freelanceItems"></AccordionInfo>
 </template>
 
 <script>
+import AccordionInfo from "@/components/AccordionInfo.vue";
+
 export default {
   name: "FreelanceView",
+  components: {
+    AccordionInfo,
+  },
+  data() {
+    return {
+      freelanceItems: [
+        {
+          id: "1",
+          header: "Header 1",
+          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+        },
+        {
+          id: "2",
+          header: "Header2",
+          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+        },
+      ],
+    };
+  },
 };
 </script>
 
