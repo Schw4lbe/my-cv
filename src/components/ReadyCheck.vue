7d08c71c158d3863cef40cfdb9e725372b06c707
TapeMate <admin@tapemate.onmicrosoft.com>
2023-08-01 22:27:00 +0200

    reworked readycheck.

--- src/components/ReadyCheck.vue
+++ src/components/ReadyCheck.vue
@@ -16,6 +16,9 @@
           v-model="input.checked"
         />
         <label class="input-label" :for="input.label">{{ input.label }}</label>
+        <div class="icon-container">
+          <i :class="input.icon" class="input-icon"></i>
+        </div>
       </div>
 
       <div class="button-container">
@@ -24,7 +27,6 @@
     </form>
   </div>
 </template>
-
 <script>
 import IntroScene from "@/components/IntroScene.vue";
 
@@ -43,63 +45,3 @@ export default {
   },
 };
 </script>
-
-<style scoped>
-.intro-container {
-  position: absolute;
-}
-
-.readycheck-container {
-  display: flex;
-  justify-content: center;
-  padding-top: 30%;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-
-  animation-name: fadeInReadyCheck;
-  animation-iteration-count: 1;
-  animation-fill-mode: forwards;
-  animation-timing-function: ease-in;
-  animation-duration: 1s;
-  animation-delay: 22s;
-}
-
-@keyframes fadeInReadyCheck {
-  0% {
-    opacity: 0;
-  }
-  100% {
-    opacity: 1;
-  }
-}
-
-.input-container {
-  display: flex;
-  justify-content: flex-start;
-}
-.input-label {
-  color: white;
-  padding: 1rem;
-  margin-left: 30px;
-}
-
-.input-checkbox {
-  display: block;
-  margin: 1rem;
-  background: white;
-  width: 20px;
-  height: 20px;
-  opacity: 1;
-  z-index: 1;
-}
-
-button {
-  padding: 1rem;
-  margin: 2rem;
-}
-
-.hide {
-  display: none !important;
-}
-</style>
