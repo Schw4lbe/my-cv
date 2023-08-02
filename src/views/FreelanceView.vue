47d9aff95886843ffe1162b30db1148f13b458a1
TapeMate <admin@tapemate.onmicrosoft.com>
2023-08-02 16:03:36 +0200

    entered content to freelanceview

--- src/views/FreelanceView.vue
+++ src/views/FreelanceView.vue
@@ -16,13 +16,35 @@ export default {
       freelanceItems: [
         {
           id: "1",
-          header: "Header 1",
-          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+          header: "Besinnung & Neuorientierung",
+          period: "10/2020 - 10/2021",
+          subtopics: [
+            "Aufbau eines Twitch.tv Livestream Kanals (Affiliate Status), Live DJ-ing & Vj-ing",
+            "Erste Einblicke Game Development C-Sharp auf Unity Engine",
+            "Einarbeitung in Character Animation mittels Blender",
+            "Eruierung möglicher Weiterbildungsmaßnahmen",
+            "Austausch mit diversen Schulungsunternehmen",
+          ],
+          success:
+            "Twitch Livestream vor knapp 1000 Viewern mit Schallplatten - Hosting durch bekannten Künstler",
+          logo: "",
+          media: "",
         },
         {
           id: "2",
-          header: "Header2",
-          subtopics: ["Subtopic 1", "Subtopic 2", "Subtopic 3"],
+          header: "Eventmanagement & Musiker - Münchner Grammophon e. K.",
+          period: "10/2014 - 09/2016",
+          subtopics: [
+            "Planung & Organisation von Veranstaltungen 50 – 1.200 Teilnehmer",
+            "Tätigkeit als Schallplattenunterhalter in diversen Lokalitäten in München",
+            "Vernetzung von Veranstaltungskollektiven in Deutschland / Österreich",
+            "Abschluss von Kooperationen zwischen Betreibern & Dienstleistern",
+            "Werbetätigkeiten & Bearbeitung von Onlinepräsenzen",
+          ],
+          success:
+            "Drei Fragezeichen Festival Feierwerk München (ca. 1.200 Besucher)",
+          logo: "",
+          media: "",
         },
       ],
     };
