a14e183de60fa780210da86b23d8c308543c43dd
TapeMate <admin@tapemate.onmicrosoft.com>
2023-08-02 23:48:17 +0200

    updated dummy content.

--- src/components/ReferenceContainer.vue
+++ src/components/ReferenceContainer.vue
@@ -18,6 +18,24 @@
         <img src="../assets/skills-dummy.gif" alt="dummy" />
       </div>
     </div>
+    <div class="reference-container-reverse">
+      <div class="media-container-reverse">
+        <img src="../assets/skills-dummy.gif" alt="dummy" />
+      </div>
+      <div class="description-container">
+        <h3 class="reference-header">Header</h3>
+        <p class="reference-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          eveniet eaque saepe eligendi ad. Commodi quasi corrupti fugit
+          exercitationem tempora. Odio fugiat nulla harum id deserunt porro
+          commodi voluptates consectetur. Nulla quibusdam veniam ex itaque dicta
+          alias officia iure reiciendis quod asperiores. Illum magni odit
+          quidem, qui cum cupiditate eveniet reiciendis omnis deserunt
+          aspernatur. Dignissimos quisquam perferendis explicabo reprehenderit
+          hic.
+        </p>
+      </div>
+    </div>
     <div class="reference-container">
       <div class="description-container">
         <h3 class="reference-header">Header</h3>
@@ -36,6 +54,24 @@
         <img src="../assets/skills-dummy.gif" alt="dummy" />
       </div>
     </div>
+    <div class="reference-container-reverse">
+      <div class="media-container-reverse">
+        <img src="../assets/skills-dummy.gif" alt="dummy" />
+      </div>
+      <div class="description-container">
+        <h3 class="reference-header">Header</h3>
+        <p class="reference-info">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+          eveniet eaque saepe eligendi ad. Commodi quasi corrupti fugit
+          exercitationem tempora. Odio fugiat nulla harum id deserunt porro
+          commodi voluptates consectetur. Nulla quibusdam veniam ex itaque dicta
+          alias officia iure reiciendis quod asperiores. Illum magni odit
+          quidem, qui cum cupiditate eveniet reiciendis omnis deserunt
+          aspernatur. Dignissimos quisquam perferendis explicabo reprehenderit
+          hic.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -51,29 +87,39 @@ export default {
 <style scoped>
 .reference-grid-container {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 1fr;
 }
 
-.reference-container {
+.reference-container,
+.reference-container-reverse {
   display: grid;
-  grid-template-columns: 1fr 1fr;
+  grid-template-columns: 5fr 7fr;
   padding: 1rem;
+  border-top: 1px solid black;
+  margin-bottom: 30px;
 }
 
-.description-container {
-  display: flex;
-  justify-content: flex-start;
-  flex-wrap: wrap;
+.reference-container-reverse {
+  grid-template-columns: 7fr 5fr;
 }
 
 .reference-info {
   text-align: left;
 }
 
-.media-container {
+.media-container,
+.media-container-reverse {
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
   align-items: center;
+}
+
+.media-container-reverse {
+  justify-content: flex-start;
+}
+
+h3 {
+  padding: 1rem;
 }
 
 img {
