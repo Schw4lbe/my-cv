b15e7251a01449886f963ec38552c402e03226ef
TapeMate <admin@tapemate.onmicrosoft.com>
2023-08-02 15:21:24 +0200

    embedded auto color assignment to skill rating.

--- src/components/PersonalData.vue
+++ src/components/PersonalData.vue
@@ -1,29 +1,36 @@
 <template>
   <div class="data-container">
-    <!-- <div class="personal-data-container">
+    <div class="personal-data-container">
       <div class="propertie-container">
+        <h2>Stärken</h2>
         <ul>
           <li v-for="prop in properties" :key="prop.id">{{ prop.name }}</li>
         </ul>
       </div>
+
       <div class="hobby-container">
+        <h2>Hobbies</h2>
         <ul>
           <li v-for="hobby in hobbies" :key="hobby.id">{{ hobby.name }}</li>
         </ul>
       </div>
-    </div> -->
+    </div>
 
     <div class="skill-data-container">
+      <h2>Skill Matrix Web-Development:</h2>
       <div v-for="skill in skills" :key="skill.id" class="skill-container">
         <i :class="skill.icon" class="skill-icon"></i>
         <span class="skill-name">{{ skill.name }}</span>
+        <span class="skill-rating">{{ skill.rating }} / 10</span>
         <div class="progress-bar">
           <div
-            :style="{ width: getProgressWidth(skill.rating) + '%' }"
+            :style="{
+              width: getProgressWidth(skill.rating) + '%',
+              backgroundColor: setProgressBarColor(skill.rating),
+            }"
             class="progress"
           ></div>
         </div>
-        <span class="skill-rating">{{ skill.rating }} / 10</span>
       </div>
     </div>
   </div>
@@ -37,10 +44,51 @@ export default {
     hobbies: Array,
     skills: Array,
   },
+  data() {
+    return {
+      colors: [
+        { id: "0", code: "#025bff" },
+        { id: "1", code: "#1571ff" },
+        { id: "2", code: "#2797ff" },
+        { id: "3", code: "#3abeff" },
+        { id: "4", code: "#4fd4ff" },
+        { id: "5", code: "#66eaff" },
+        { id: "6", code: "#7cffff" },
+        { id: "7", code: "#93ffff" },
+        { id: "8", code: "#aaffff" },
+        { id: "10", code: "#ffffff" },
+      ],
+    };
+  },
   computed: {
     getProgressWidth() {
-      return (rating) => {
+      return function (rating) {
         return (rating / 10) * 100;
+      };
+    },
+    setProgressBarColor() {
+      return function (rating) {
+        if (rating === "1") {
+          return this.colors[0].code;
+        } else if (rating === "2") {
+          return this.colors[1].code;
+        } else if (rating === "3") {
+          return this.colors[2].code;
+        } else if (rating === "4") {
+          return this.colors[3].code;
+        } else if (rating === "5") {
+          return this.colors[4].code;
+        } else if (rating === "6") {
+          return this.colors[5].code;
+        } else if (rating === "7") {
+          return this.colors[6].code;
+        } else if (rating === "8") {
+          return this.colors[7].code;
+        } else if (rating === "9") {
+          return this.colors[8].code;
+        } else if (rating === "10") {
+          return this.colors[9].code;
+        }
       };
     },
   },
--- src/views/PersonView.vue
+++ src/views/PersonView.vue
@@ -19,8 +19,55 @@ export default {
   data() {
     return {
       skillsArray: [
-        { id: "1", icon: "fa-brands fa-html5", name: "html", rating: "9" },
-        { id: "2", icon: "fa-brands fa-css3-alt", name: "css", rating: "7" },
+        { id: "1", icon: "fa-brands fa-html5", name: "html 5", rating: "9" },
+        { id: "2", icon: "fa-brands fa-css3-alt", name: "css 3", rating: "8" },
+        { id: "3", icon: "fa-brands fa-sass", name: "scss", rating: "8" },
+        {
+          id: "4",
+          icon: "fa-brands fa-bootstrap",
+          name: "bootstrap",
+          rating: "7",
+        },
+        { id: "5", icon: "fa-brands fa-code", name: "quasar", rating: "3" },
+        { id: "6", icon: "fa-brands fa-uikit", name: "ux / ui", rating: "5" },
+        {
+          id: "7",
+          icon: "fa-brands fa-js",
+          name: "javascript es6",
+          rating: "6",
+        },
+        {
+          id: "8",
+          icon: "fa-brands fa-js",
+          name: "javascript dom",
+          rating: "8",
+        },
+        {
+          id: "9",
+          icon: "fa-sharp fa-light fa-superscript",
+          name: "typescript",
+          rating: "2",
+        },
+        {
+          id: "10",
+          icon: "fa-brands fa-node-js",
+          name: "node.js",
+          rating: "3",
+        },
+        { id: "11", icon: "fa-solid fa-e", name: "express", rating: "1" },
+        { id: "12", icon: "fa-brands fa-react", name: "react", rating: "2" },
+        {
+          id: "13",
+          icon: "fa-brands fa-vuejs",
+          name: "vue js 2 & 3",
+          rating: "6",
+        },
+        {
+          id: "14",
+          icon: "fa-regular fa-window-restore",
+          name: "browserstack",
+          rating: "6",
+        },
       ],
       propertiesArray: [
         { id: "1", name: "Eigenmotivation" },
