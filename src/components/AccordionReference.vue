c7fe54011b0390a91922f1bcbcec459c8dca5c50
TapeMate <admin@tapemate.onmicrosoft.com>
2023-08-05 16:16:59 +0200

    created new component.

---
+++ src/components/AccordionReference.vue
@@ -0,0 +1,11 @@
+<template>
+  <h1>Dummy Header</h1>
+</template>
+
+<script>
+export default {
+  name: "AccordionReference",
+};
+</script>
+
+<style scoped></style>
--- src/components/ReferenceContainer.vue
+++ src/components/ReferenceContainer.vue
@@ -25,60 +25,6 @@
         <img src="../assets/unity-demo.gif" alt="dummy" />
       </div>
     </div>
-    <div class="reference-container-reverse">
-      <div class="media-container-reverse">
-        <img src="../assets/skills-dummy.gif" alt="dummy" />
-      </div>
-      <div class="description-container">
-        <h3 class="reference-header">Header</h3>
-        <p class="reference-info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          eveniet eaque saepe eligendi ad. Commodi quasi corrupti fugit
-          exercitationem tempora. Odio fugiat nulla harum id deserunt porro
-          commodi voluptates consectetur. Nulla quibusdam veniam ex itaque dicta
-          alias officia iure reiciendis quod asperiores. Illum magni odit
-          quidem, qui cum cupiditate eveniet reiciendis omnis deserunt
-          aspernatur. Dignissimos quisquam perferendis explicabo reprehenderit
-          hic.
-        </p>
-      </div>
-    </div>
-    <div class="reference-container">
-      <div class="description-container">
-        <h3 class="reference-header">Header</h3>
-        <p class="reference-info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          eveniet eaque saepe eligendi ad. Commodi quasi corrupti fugit
-          exercitationem tempora. Odio fugiat nulla harum id deserunt porro
-          commodi voluptates consectetur. Nulla quibusdam veniam ex itaque dicta
-          alias officia iure reiciendis quod asperiores. Illum magni odit
-          quidem, qui cum cupiditate eveniet reiciendis omnis deserunt
-          aspernatur. Dignissimos quisquam perferendis explicabo reprehenderit
-          hic.
-        </p>
-      </div>
-      <div class="media-container">
-        <img src="../assets/skills-dummy.gif" alt="dummy" />
-      </div>
-    </div>
-    <div class="reference-container-reverse">
-      <div class="media-container-reverse">
-        <img src="../assets/skills-dummy.gif" alt="dummy" />
-      </div>
-      <div class="description-container">
-        <h3 class="reference-header">Header</h3>
-        <p class="reference-info">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
-          eveniet eaque saepe eligendi ad. Commodi quasi corrupti fugit
-          exercitationem tempora. Odio fugiat nulla harum id deserunt porro
-          commodi voluptates consectetur. Nulla quibusdam veniam ex itaque dicta
-          alias officia iure reiciendis quod asperiores. Illum magni odit
-          quidem, qui cum cupiditate eveniet reiciendis omnis deserunt
-          aspernatur. Dignissimos quisquam perferendis explicabo reprehenderit
-          hic.
-        </p>
-      </div>
-    </div>
   </div>
 </template>
 
