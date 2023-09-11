87de78bc14b81c4cdb296cc450bfb07efa819d03
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-11 22:56:36 +0200

    embedded gulp for prefixes. updatet techstack.

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require("gulp");
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+
+function applyAutoprefixer() {
+  return gulp
+    .src("public/css/main.css")
+    .pipe(postcss([autoprefixer()]))
+    .pipe(gulp.dest("public/css"));
+}
+
+exports.default = applyAutoprefixer;
