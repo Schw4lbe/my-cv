89f38c5cc13758c7ed1e17cab337369920cfd51f
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-15 21:38:55 +0200

    refactored vuex store into modules.

--- README.md
+++ README.md
@@ -31,8 +31,11 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 ### BACKLOG
 
 - Sprachauswahl (Deutsch Englisch)
+  -> Neuer Button im Login screen für Auswahl Sprache vor Besuch der Seite
+  -> Outsourcing aller Daten weg von hard coding und weg von components
 - Alle öffnen Button in Reference
 - Refference pot. vereinheitlichen
 - alle schließen Accordion x und . weg
 - JWT Funktion und Umfang im Backend verifizieren
 - Login logisch von App.vue trennen (eigene Komponente und logic für request verschieben)
+- Refactoring Komponentennamen
--- src/store/index.js
+++ src/store/index.js
@@ -1,65 +1,12 @@
 import { createStore } from "vuex";
+import state from "@/store/modules/state";
+import mutations from "@/store/modules/mutations";
+import getters from "@/store/modules/getters";
 
 const store = createStore({
-  state: {
-    loginSuccess: localStorage.getItem("loginSuccess") === "true" || false,
-    hideLogin: localStorage.getItem("hideLogin") === "true" || false,
-    elementHidden: localStorage.getItem("elementHidden") === "true" || false,
-    cvMainVisible: localStorage.getItem("cvMainVisible") === "true" || false,
-    serverError: localStorage.getItem("serverError") === "true" || false,
-    waitingForServer:
-      localStorage.getItem("waitingForServer") === "true" || false,
-  },
-
-  mutations: {
-    waitingForServerResponse(state) {
-      state.waitingForServer = true;
-      localStorage.setItem("waitingForServer", "true");
-    },
-    notWaitingForServerResponse(state) {
-      state.waitingForServer = false;
-      localStorage.setItem("waitingForServer", "false");
-    },
-    serverErrorTrue(state) {
-      state.serverError = true;
-      localStorage.setItem("serverError", "true");
-    },
-    serverErrorFalse(state) {
-      state.serverError = false;
-      localStorage.setItem("serverError", "false");
-    },
-    loginSuccess(state) {
-      state.loginSuccess = true;
-      localStorage.setItem("loginSuccess", "true");
-    },
-    hideLogin(state) {
-      state.hideLogin = true;
-      localStorage.setItem("hideLogin", "true");
-    },
-    hideElement(state) {
-      state.elementHidden = true;
-      localStorage.setItem("elementHidden", "true");
-    },
-    showElement(state) {
-      state.elementHidden = false;
-      localStorage.setItem("elementHidden", "false");
-    },
-    showCvMain(state) {
-      state.cvMainVisible = true;
-      localStorage.setItem("cvMainVisible", "true");
-    },
-  },
-
-  actions: {},
-
-  getters: {
-    isWaitingForServer: (state) => state.waitingForServer,
-    hasServerError: (state) => state.serverError,
-    isLoginSuccess: (state) => state.loginSuccess,
-    isLoginHidden: (state) => state.hideLogin,
-    isElementHidden: (state) => state.elementHidden,
-    isCvMainVisible: (state) => state.cvMainVisible,
-  },
+  state,
+  mutations,
+  getters,
 });
 
 export default store;
