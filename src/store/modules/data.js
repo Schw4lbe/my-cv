9cf26d36962d3a5f6dfb902f69718cfa3efb629a
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-16 18:51:19 +0200

    outsourced references data.

--- src/store/modules/data.js
+++ src/store/modules/data.js
@@ -4,6 +4,12 @@ import Logo4 from "@/assets/img/logo-gfn.svg";
 import Logo5 from "@/assets/img/logo-tapemate.png";
 import Logo6 from "@/assets/img/logo-grammophon.png";
 
+import img01 from "@/assets/img/kundenprojekt-demo.gif";
+import img02 from "@/assets/img/homepage-demo.gif";
+import img03 from "@/assets/img/contactform-demo.gif";
+import img04 from "@/assets/img/weatherapp-demo.gif";
+import img05 from "@/assets/img/unity-demo.gif";
+
 export const data = {
   de: {
     introContent: [
@@ -152,6 +158,83 @@ export const data = {
         logo: Logo6,
         media: "",
         link: "https://soundcloud.com/muenchner-grammophon",
+      },
+    ],
+    referenceArray: [
+      {
+        id: "01",
+        name: "Wetter APP",
+        content: {
+          text: "Zur Übung in der Einbindung und im Umgang von API's als auch im Schreiben von Node.js, habe ich mehrere Youtube Tutorials vollzogen. Neben dieser Anwendung habe ich zudem einen kleinen Chatbot und einen Taskmanager erstellt. Die Anwendung basiert auf der OpenWeatherAPI.",
+          subheader: "Tech Stack:",
+          notes: [
+            "VUE.JS 3, HTML, CSS, JavaScript ES6, Node.js, Express",
+            "VUE Router, ESLint, Prettier, Visual Studio Code",
+            "OpenWeatherAPI: https://openweathermap.org",
+          ],
+          img: img04,
+        },
+      },
+      {
+        id: "02",
+        name: "Dummy Homepage",
+        content: {
+          text: "Im Lauf der letzten Monate habe ich mir eine Dummy Homepage aufgebaut. Wenn ich eine neue Technologie für mich entdecke oder ein Design testen möchte, kann auf diesen Build zurückgreifen. Die Dummy Page gibt mir die Möglichkeit schnell und effizient zu testen und lernen.",
+          subheader: "Tech Stack:",
+          notes: [
+            "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
+            "VUE Router, Bootstrap via CDN, ESLint, Prettier",
+            "Visual Studio Code, LiveSASS Compiler",
+            "JSON-Server als Fake Backend",
+          ],
+          img: img02,
+        },
+      },
+      {
+        id: "03",
+        name: "Kontakt Formular",
+        content: {
+          text: "Erstellung eigenes Kontaktformular zur Festigung neu erworbenem Wissens, sowie kreative Spielwiese für dieverse Styling Ideen.",
+          subheader: "Tech Stack & Details:",
+          notes: [
+            "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
+            "Verifzierung Telefonnummer und E-Mail Adresse via Regex",
+            "Eigener Captcha samt Honeypot",
+            "Captcha Lösungswort & Honeypot durch CSS manipuliert",
+          ],
+          img: img03,
+        },
+      },
+      {
+        id: "04",
+        name: "Kundenprojekt",
+        content: {
+          text: "Kundenprojekt für Stadtwerke Lauterbach. Umsetzung einer online Bestellstrecke mittels VUE.JS 2.x, TypeScript, SCSS & JSON-Schema.",
+          subheader: "Tech Stack & Herausforderungen:",
+          notes: [
+            "VUE.JS 2.x, TypeScript, JSON-Schema",
+            "EDI: Visual Studio Code & PHP Storm",
+            "genannte Technologien waren Neuland",
+            "Kompatibilität für Geräte der letzten 6 Jahre",
+            "Komplette Anwendung läuft in einem IFrame",
+          ],
+          img: img01,
+        },
+      },
+      {
+        id: "05",
+        name: "Game Development",
+        content: {
+          text: "Meine Passion für Computerspiele hat mich dazu verleitet, mir mittels Unity Dokumentationen, C# Dokumentationen, Youtube Tutorials, Google und Chat GPT das Programmieren in C# auf Basis der Unity Engine selbst bei zu bringen. Das Freizeitprojekt betrachte ich als Work in Progress und wird von mir kontinuierlich weiter entwickelt.",
+          subheader: "Randnotizen & Tech Stack:",
+          notes: [
+            "Unity Editor & Unity Engine",
+            "C# in Microsoft Visual Studio 2022",
+            "Gameconcept in Markdown",
+            "Item & Crafting Sprites samt Table",
+          ],
+          img: img05,
+        },
       },
     ],
   },
@@ -303,6 +386,83 @@ export const data = {
         logo: Logo6,
         media: "",
         link: "https://soundcloud.com/muenchner-grammophon",
+      },
+    ],
+    referenceArray: [
+      {
+        id: "01",
+        name: "Wetter APP",
+        content: {
+          text: "Zur Übung in der Einbindung und im Umgang von API's als auch im Schreiben von Node.js, habe ich mehrere Youtube Tutorials vollzogen. Neben dieser Anwendung habe ich zudem einen kleinen Chatbot und einen Taskmanager erstellt. Die Anwendung basiert auf der OpenWeatherAPI.",
+          subheader: "Tech Stack:",
+          notes: [
+            "VUE.JS 3, HTML, CSS, JavaScript ES6, Node.js, Express",
+            "VUE Router, ESLint, Prettier, Visual Studio Code",
+            "OpenWeatherAPI: https://openweathermap.org",
+          ],
+          img: img04,
+        },
+      },
+      {
+        id: "02",
+        name: "Dummy Homepage",
+        content: {
+          text: "Im Lauf der letzten Monate habe ich mir eine Dummy Homepage aufgebaut. Wenn ich eine neue Technologie für mich entdecke oder ein Design testen möchte, kann auf diesen Build zurückgreifen. Die Dummy Page gibt mir die Möglichkeit schnell und effizient zu testen und lernen.",
+          subheader: "Tech Stack:",
+          notes: [
+            "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
+            "VUE Router, Bootstrap via CDN, ESLint, Prettier",
+            "Visual Studio Code, LiveSASS Compiler",
+            "JSON-Server als Fake Backend",
+          ],
+          img: img02,
+        },
+      },
+      {
+        id: "03",
+        name: "Kontakt Formular",
+        content: {
+          text: "Erstellung eigenes Kontaktformular zur Festigung neu erworbenem Wissens, sowie kreative Spielwiese für dieverse Styling Ideen.",
+          subheader: "Tech Stack & Details:",
+          notes: [
+            "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
+            "Verifzierung Telefonnummer und E-Mail Adresse via Regex",
+            "Eigener Captcha samt Honeypot",
+            "Captcha Lösungswort & Honeypot durch CSS manipuliert",
+          ],
+          img: img03,
+        },
+      },
+      {
+        id: "04",
+        name: "Kundenprojekt",
+        content: {
+          text: "Kundenprojekt für Stadtwerke Lauterbach. Umsetzung einer online Bestellstrecke mittels VUE.JS 2.x, TypeScript, SCSS & JSON-Schema.",
+          subheader: "Tech Stack & Herausforderungen:",
+          notes: [
+            "VUE.JS 2.x, TypeScript, JSON-Schema",
+            "EDI: Visual Studio Code & PHP Storm",
+            "genannte Technologien waren Neuland",
+            "Kompatibilität für Geräte der letzten 6 Jahre",
+            "Komplette Anwendung läuft in einem IFrame",
+          ],
+          img: img01,
+        },
+      },
+      {
+        id: "05",
+        name: "Game Development",
+        content: {
+          text: "Meine Passion für Computerspiele hat mich dazu verleitet, mir mittels Unity Dokumentationen, C# Dokumentationen, Youtube Tutorials, Google und Chat GPT das Programmieren in C# auf Basis der Unity Engine selbst bei zu bringen. Das Freizeitprojekt betrachte ich als Work in Progress und wird von mir kontinuierlich weiter entwickelt.",
+          subheader: "Randnotizen & Tech Stack:",
+          notes: [
+            "Unity Editor & Unity Engine",
+            "C# in Microsoft Visual Studio 2022",
+            "Gameconcept in Markdown",
+            "Item & Crafting Sprites samt Table",
+          ],
+          img: img05,
+        },
       },
     ],
   },
--- src/views/ReferenceView.vue
+++ src/views/ReferenceView.vue
@@ -1,105 +1,33 @@
 <template>
   <div class="reference-wrapper">
     <div class="reference-data-container">
-      <h3 class="page-title">Referenzen</h3>
-      <AccordionReference :items="itemData" />
+      <h3 class="page-title">{{ getHeader }}</h3>
+      <AccordionReference :items="referenceArray" />
     </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import AccordionReference from "@/components/AccordionReference.vue";
-import img01 from "@/assets/img/kundenprojekt-demo.gif";
-import img02 from "@/assets/img/homepage-demo.gif";
-import img03 from "@/assets/img/contactform-demo.gif";
-import img04 from "@/assets/img/weatherapp-demo.gif";
-import img05 from "@/assets/img/unity-demo.gif";
 
 export default {
   name: "ReferenceView",
   components: {
     AccordionReference,
   },
-  data() {
-    return {
-      itemData: [
-        {
-          id: "01",
-          name: "Wetter APP",
-          content: {
-            text: "Zur Übung in der Einbindung und im Umgang von API's als auch im Schreiben von Node.js, habe ich mehrere Youtube Tutorials vollzogen. Neben dieser Anwendung habe ich zudem einen kleinen Chatbot und einen Taskmanager erstellt. Die Anwendung basiert auf der OpenWeatherAPI.",
-            subheader: "Tech Stack:",
-            notes: [
-              "VUE.JS 3, HTML, CSS, JavaScript ES6, Node.js, Express",
-              "VUE Router, ESLint, Prettier, Visual Studio Code",
-              "OpenWeatherAPI: https://openweathermap.org",
-            ],
-            img: img04,
-          },
-        },
-        {
-          id: "02",
-          name: "Dummy Homepage",
-          content: {
-            text: "Im Lauf der letzten Monate habe ich mir eine Dummy Homepage aufgebaut. Wenn ich eine neue Technologie für mich entdecke oder ein Design testen möchte, kann auf diesen Build zurückgreifen. Die Dummy Page gibt mir die Möglichkeit schnell und effizient zu testen und lernen.",
-            subheader: "Tech Stack:",
-            notes: [
-              "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
-              "VUE Router, Bootstrap via CDN, ESLint, Prettier",
-              "Visual Studio Code, LiveSASS Compiler",
-              "JSON-Server als Fake Backend",
-            ],
-            img: img02,
-          },
-        },
-        {
-          id: "03",
-          name: "Kontakt Formular",
-          content: {
-            text: "Erstellung eigenes Kontaktformular zur Festigung neu erworbenem Wissens, sowie kreative Spielwiese für dieverse Styling Ideen.",
-            subheader: "Tech Stack & Details:",
-            notes: [
-              "VUE.JS 3, HTML, SCSS, JavaScript ES6, JSON-Server",
-              "Verifzierung Telefonnummer und E-Mail Adresse via Regex",
-              "Eigener Captcha samt Honeypot",
-              "Captcha Lösungswort & Honeypot durch CSS manipuliert",
-            ],
-            img: img03,
-          },
-        },
-        {
-          id: "04",
-          name: "Kundenprojekt",
-          content: {
-            text: "Kundenprojekt für Stadtwerke Lauterbach. Umsetzung einer online Bestellstrecke mittels VUE.JS 2.x, TypeScript, SCSS & JSON-Schema.",
-            subheader: "Tech Stack & Herausforderungen:",
-            notes: [
-              "VUE.JS 2.x, TypeScript, JSON-Schema",
-              "EDI: Visual Studio Code & PHP Storm",
-              "genannte Technologien waren Neuland",
-              "Kompatibilität für Geräte der letzten 6 Jahre",
-              "Komplette Anwendung läuft in einem IFrame",
-            ],
-            img: img01,
-          },
-        },
-        {
-          id: "05",
-          name: "Game Development",
-          content: {
-            text: "Meine Passion für Computerspiele hat mich dazu verleitet, mir mittels Unity Dokumentationen, C# Dokumentationen, Youtube Tutorials, Google und Chat GPT das Programmieren in C# auf Basis der Unity Engine selbst bei zu bringen. Das Freizeitprojekt betrachte ich als Work in Progress und wird von mir kontinuierlich weiter entwickelt.",
-            subheader: "Randnotizen & Tech Stack:",
-            notes: [
-              "Unity Editor & Unity Engine",
-              "C# in Microsoft Visual Studio 2022",
-              "Gameconcept in Markdown",
-              "Item & Crafting Sprites samt Table",
-            ],
-            img: img05,
-          },
-        },
-      ],
-    };
+  computed: {
+    ...mapGetters(["selectedLanguage"]),
+
+    referenceArray() {
+      return this.$store.state.contentData[this.selectedLanguage]
+        .referenceArray;
+    },
+
+    getHeader() {
+      return this.$store.state.contentData[this.selectedLanguage].menuItems[4]
+        .name;
+    },
   },
 };
 </script>
