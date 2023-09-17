9e2051233efe35bd85885e2bc443a0dd8455bc54
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-17 15:19:05 +0200

    enabled live language change function.

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="toggle-radio">
-    <input type="radio" name="rdo" id="de" checked />
-    <input type="radio" name="rdo" id="en" />
+    <input @click="setLanguageToDE" type="radio" name="rdo" id="de" checked />
+    <input @click="setLanguageToEN" type="radio" name="rdo" id="en" />
     <div class="switch">
       <label for="de">DE </label>
       <span class="separator">|</span>
@@ -12,7 +12,23 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex";
+
 export default {
   name: "ToggleButton",
+
+  methods: {
+    ...mapMutations(["setLanguageDE", "setLanguageEN"]),
+
+    setLanguageToDE() {
+      console.log("set language: DE");
+      this.setLanguageDE();
+    },
+
+    setLanguageToEN() {
+      console.log("set language: EN");
+      this.setLanguageEN();
+    },
+  },
 };
 </script>
