6cfc087c432ad98dcd189a81c62dd74113a1ebf6
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-17 17:04:41 +0200

    togglebutton now gets checked state relating on selectedLanguage cookie value.

--- src/components/ToggleButton.vue
+++ src/components/ToggleButton.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="toggle-radio">
-    <input @click="setLanguageToDE" type="radio" name="rdo" id="de" checked />
-    <input @click="setLanguageToEN" type="radio" name="rdo" id="en" />
+    <input
+      @click="setLanguageToDE"
+      type="radio"
+      name="rdo"
+      id="de"
+      :checked="selectedLanguage === 'de'"
+    />
+    <input
+      @click="setLanguageToEN"
+      type="radio"
+      name="rdo"
+      id="en"
+      :checked="selectedLanguage === 'en'"
+    />
     <div class="switch">
       <label for="de">DE </label>
       <span class="separator">|</span>
@@ -12,10 +24,14 @@
 </template>
 
 <script>
-import { mapMutations } from "vuex";
+import { mapMutations, mapGetters } from "vuex";
 
 export default {
   name: "ToggleButton",
+
+  computed: {
+    ...mapGetters(["selectedLanguage"]),
+  },
 
   methods: {
     ...mapMutations(["setLanguageDE", "setLanguageEN"]),
