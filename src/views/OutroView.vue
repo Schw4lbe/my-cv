d0a15e67e6b0a05beea56c949da1c64cd27070d6
TapeMate <admin@tapemate.onmicrosoft.com>
2023-09-17 11:57:00 +0200

    outsourced outro header.

--- src/views/OutroView.vue
+++ src/views/OutroView.vue
@@ -1,16 +1,26 @@
 <template>
   <div class="outro-wrapper">
-    <h3 class="page-title">Schlusswort</h3>
+    <h3 class="page-title">{{ setHeader }}</h3>
     <OutroSummit />
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import OutroSummit from "@/components/OutroSummit.vue";
 
 export default {
   name: "OutroView",
   components: OutroSummit,
+
+  computed: {
+    ...mapGetters(["selectedLanguage"]),
+
+    setHeader() {
+      return this.$store.state.contentData[this.selectedLanguage].menuItems[5]
+        .name;
+    },
+  },
 };
 </script>
 
