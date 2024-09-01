500eaa94eec48fe15428963dfe068b846431ad81
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-01 16:45:13 +0200

    addec on method to assign class to highlight item in the center of the viewport. set active method has to be reviewed, two items can be active at the same time leading to toggle state and random page movement.

--- src/components/DevComponent.vue
+++ src/components/DevComponent.vue
@@ -2,10 +2,11 @@
   <div class="dev-wrapper">
     <div class="dev timeline-container">
       <div
-        v-for="item in timelineItems"
+        v-for="(item, itemIndex) in timelineItems"
         :key="item.id"
         :id="'timeline-item' + item.id"
         class="timeline-item"
+        :class="{ 'timeline-focus': itemIndex === 0 }"
       >
         <div class="timeline-marker"></div>
         <div class="timeline-period">{{ item.period }}</div>
@@ -13,6 +14,7 @@
           <h3 class="timeline-header" @click="setTimelineActive">
             {{ item.header }}
           </h3>
+          <p class="teaser">[ klick für mehr Infos ]</p>
           <ul class="subtopics">
             <li
               v-for="(topic, index) in item.subtopics"
@@ -34,6 +36,9 @@
 <script>
 export default {
   name: "DevComponent",
+  props: {
+    timelineItems: Array,
+  },
 
   data() {
     return {
@@ -41,12 +46,49 @@ export default {
     };
   },
 
-  props: {
-    timelineItems: Array,
+  mounted() {
+    window.addEventListener("scroll", this.setTimelineFocus);
+  },
+
+  beforeUnmount() {
+    window.removeEventListener("scroll", this.setTimelineFocus);
   },
 
   methods: {
+    setTimelineFocus() {
+      const focusables = this.$el.querySelectorAll(".timeline-item");
+      let closestElement = null;
+      let closestDistance = Infinity;
+      const viewportMiddle = window.innerHeight / 2;
+
+      focusables.forEach((focusable) => {
+        const rect = focusable.getBoundingClientRect();
+        const elementMiddle = rect.top + rect.height / 2;
+        const distanceToMiddle = Math.abs(viewportMiddle - elementMiddle);
+
+        if (distanceToMiddle < closestDistance) {
+          closestDistance = distanceToMiddle;
+          closestElement = focusable;
+        }
+      });
+
+      focusables.forEach((focusable) => {
+        if (focusable.classList.contains("timeline-active")) {
+          console.log("active timeline");
+        } else {
+          focusable.classList.remove("timeline-focus");
+        }
+      });
+
+      if (closestElement) {
+        closestElement.classList.add("timeline-focus");
+      }
+    },
+
     setTimelineActive(e) {
+      if (!e.target) {
+        return;
+      }
       const timeline = e.target.closest(".timeline-item");
 
       if (timeline && timeline !== this.lastActiveTimeline) {
