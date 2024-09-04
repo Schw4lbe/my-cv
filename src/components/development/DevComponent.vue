ba3d9cf2b258fb088146bd59d9e663fa931a9c5a
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-04 18:14:16 +0200

    designed image on load animation trigger for modal demo. deployment for live testing.

--- src/components/development/DevComponent.vue
+++ src/components/development/DevComponent.vue
@@ -1,70 +1,49 @@
 <template>
   <div class="dev">
-    <nav class="navbar navbar-dark navbar-expand-md bg-dark fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Offcanvas navbar</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="offcanvas"
-          data-bs-target="#offcanvasNavbar"
-          aria-controls="offcanvasNavbar"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div
-          class="offcanvas bg-dark offcanvas-end"
-          tabindex="-1"
-          id="offcanvasNavbar"
-          aria-labelledby="offcanvasNavbarLabel"
-        >
-          <div class="offcanvas-header">
-            <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-            <button
-              type="button"
-              class="btn-close btn btn-dark"
-              data-bs-dismiss="offcanvas"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="offcanvas-body">
-            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="#">Home</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-              </li>
-              <li class="nav-item dropdown">
-                <a
-                  class="nav-link dropdown-toggle"
-                  href="#"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul class="dropdown-menu">
-                  <li><a class="dropdown-item" href="#">Action</a></li>
-                  <li><a class="dropdown-item" href="#">Another action</a></li>
-                  <li>
-                    <hr class="dropdown-divider" />
-                  </li>
-                  <li>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <div class="image-container">
+      <button id="loadImageButton" @click="onClickLoadImage">Load Image</button>
+      <div id="loadingSpinner" class="spinner" style="display: none">
+        Loading...
       </div>
-    </nav>
+      <img id="largeImage" style="display: none" alt="Large Image" />
+    </div>
   </div>
 </template>
 <script>
+import imgSrc from "../../assets/img/hp-kv-demo.gif";
+
 export default {
   name: "DevComponent",
+
+  methods: {
+    onClickLoadImage() {
+      const imgElement = document.getElementById("largeImage");
+      const spinner = document.getElementById("loadingSpinner");
+
+      spinner.style.display = "block";
+
+      const img = new Image();
+      img.src = imgSrc;
+
+      img.onload = function () {
+        spinner.style.display = "none";
+
+        imgElement.src = img.src;
+        imgElement.style.display = "block";
+      };
+
+      img.onerror = function () {
+        spinner.style.display = "none";
+        alert("Failed to load image.");
+      };
+    },
+  },
 };
 </script>
+
+<style scoped>
+.spinner {
+  font-size: 20px;
+  color: #555;
+}
+</style>
