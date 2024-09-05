0a200ae90126deb435f66a6c2abb8f76bfaa3ded
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-05 12:21:42 +0200

    fine tuned gulp config to fix display bugs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-// const cssnano = require("cssnano");
+const cssnano = require("cssnano");
 const postcssPresetEnv = require("postcss-preset-env");
 const flexbugsFixes = require("postcss-flexbugs-fixes");
 const normalize = require("postcss-normalize");
@@ -15,11 +15,20 @@ function applyAutoprefixer() {
         postcssPresetEnv({
           stage: 0, // Enable all modern CSS features
           browsers: "last 2 versions", // Target last 2 versions of browsers
-          preserve: false, // Don't keep the original unprocessed CSS
+          preserve: true, // keep the original unprocessed CSS
         }),
         flexbugsFixes(), // Fixes known flexbox issues
         normalize(), // Adds normalization rules for consistent cross-browser rendering
-        // cssnano(), // Minifies CSS for better performance
+        cssnano({
+          preset: [
+            "default",
+            {
+              reduceInitial: false, // Avoids removing 'initial' values
+              mergeLonghand: false, // Avoids merging properties like `background`
+              discardComments: { removeAll: true }, // Removes comments
+            },
+          ],
+        }),
       ])
     )
     .pipe(gulp.dest("public/css"));
