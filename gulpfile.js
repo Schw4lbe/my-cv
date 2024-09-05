a4bdd652064fa922b8123dc0d7d4168b5a2d34d6
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-05 11:49:44 +0200

    second config.

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,8 @@ const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const cssnano = require("cssnano");
-// const postcssPresetEnv = require("postcss-preset-env");
-const flexbugsFixes = require("postcss-flexbugs-fixes");
+const postcssPresetEnv = require("postcss-preset-env");
+// const flexbugsFixes = require("postcss-flexbugs-fixes");
 const normalize = require("postcss-normalize");
 
 function applyAutoprefixer() {
@@ -12,12 +12,12 @@ function applyAutoprefixer() {
     .pipe(
       postcss([
         autoprefixer(), // Adds necessary vendor prefixes
-        // postcssPresetEnv({
-        //   stage: 0, // Enable all modern CSS features
-        //   browsers: "last 2 versions", // Target last 2 versions of browsers
-        //   preserve: false, // Don't keep the original unprocessed CSS
-        // }),
-        flexbugsFixes(), // Fixes known flexbox issues
+        postcssPresetEnv({
+          stage: 0, // Enable all modern CSS features
+          browsers: "last 2 versions", // Target last 2 versions of browsers
+          preserve: false, // Don't keep the original unprocessed CSS
+        }),
+        // flexbugsFixes(), // Fixes known flexbox issues
         normalize(), // Adds normalization rules for consistent cross-browser rendering
         cssnano(), // Minifies CSS for better performance
       ])
