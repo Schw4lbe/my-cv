59186b5acb57aa8637d4629bc6449bdfca08a6b0
TapeMate <admin@tapemate.onmicrosoft.com>
2024-09-05 11:55:08 +0200

    next test after some research.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 const gulp = require("gulp");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
-const cssnano = require("cssnano");
+// const cssnano = require("cssnano");
 const postcssPresetEnv = require("postcss-preset-env");
-// const flexbugsFixes = require("postcss-flexbugs-fixes");
+const flexbugsFixes = require("postcss-flexbugs-fixes");
 const normalize = require("postcss-normalize");
 
 function applyAutoprefixer() {
@@ -17,9 +17,9 @@ function applyAutoprefixer() {
           browsers: "last 2 versions", // Target last 2 versions of browsers
           preserve: false, // Don't keep the original unprocessed CSS
         }),
-        // flexbugsFixes(), // Fixes known flexbox issues
+        flexbugsFixes(), // Fixes known flexbox issues
         normalize(), // Adds normalization rules for consistent cross-browser rendering
-        cssnano(), // Minifies CSS for better performance
+        // cssnano(), // Minifies CSS for better performance
       ])
     )
     .pipe(gulp.dest("public/css"));
